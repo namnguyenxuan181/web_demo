edcfd65746c79e7cb61470be15473af661e4d0d2
nam nguyen <nam.nguyenxuan181@gmail.com>
2019-09-19 23:46:06 +0700

    add demo

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,11 +5,18 @@ $(document).ready(function () {
         //     console.log(data.result)
         $.post("http://localhost:8007/dl_results",
             {
-                title: 'Laptop HP 14-cK0068TU'
+                "title": $('#title').val()
                     // $('#title').val()
             },
             function (data, status) {
                 console.log( data.result);
+                var i=0
+                var res='lcs results:<br>'
+                for (i=0; i<10; i++){
+                    res+=data.result[i]+'<br>'
+                }
+                $('#lcs_result').html()=res
+                $('#lcs_result').show()
             });
     });
 });

c0a97a7c984f774a0d20509cd352035faeaac924
nam nguyen <nam.nguyenxuan181@gmail.com>
2019-09-19 23:53:13 +0700

    add result

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
     $('#lcs_result').hide();
+    $('#dl_result').hide();
     $("#bt").click(function () {
         // $.get('http://localhost:8007/dl_results', function (data, status) {
         //     console.log(data.result)
-        $.post("http://localhost:8007/dl_results",
+        $.post("http://localhost:8007/lcs_results",
             {
                 "title": $('#title').val()
                     // $('#title').val()
@@ -11,12 +12,29 @@ $(document).ready(function () {
             function (data, status) {
                 console.log( data.result);
                 var i=0
-                var res='lcs results:<br>'
-                for (i=0; i<10; i++){
-                    res+=data.result[i]+'<br>'
+                re =  data.result;
+                var res='<h3>lcs results:</h3>'
+                for (i=0; i<re.length; i++){
+                    res+=re[i]+'<br>'
                 }
                 $('#lcs_result').html()=res
                 $('#lcs_result').show()
+            });
+            $.post("http://localhost:8007/dl_results",
+            {
+                "title": $('#title').val()
+                    // $('#title').val()
+            },
+            function (data, status) {
+                console.log( data.result);
+                re =  data.result;
+                var i=0
+                var res='<h3>dl results:</h3>'
+                for (i=0; i<re.length; i++){
+                    res+=re[i]+'<br>'
+                }
+                $('#dl_result').html()=res
+                $('#dl_result').show()
             });
     });
 });

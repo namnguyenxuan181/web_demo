0cae59d25914a7b48f92fa7fb68fde39a0f2aa9e
nam.nx <nam.nx@teko.vn>
2019-09-20 13:50:04 +0700

    add all file

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
     $("#bt").click(function () {
         // $.get('http://localhost:8007/dl_results', function (data, status) {
         //     console.log(data.result)
+        $('#lcs_result').hide();
+        $('#dl_result').hide();
         $.post("http://localhost:8007/lcs_results",
             {
                 "title": $('#title').val()
@@ -11,13 +13,13 @@ $(document).ready(function () {
             },
             function (data, status) {
                 console.log( data.result);
-                var i=0
+                var i=0;
                 re =  data.result;
-                var res='<h3>lcs results:</h3>'
+                var res="<h3>lcs results:</h3>";
                 for (i=0; i<re.length; i++){
                     res+=re[i]+'<br>'
                 }
-                $('#lcs_result').html()=res
+                $('#lcs_result').html(res);
                 $('#lcs_result').show()
             });
             $.post("http://localhost:8007/dl_results",
@@ -33,7 +35,7 @@ $(document).ready(function () {
                 for (i=0; i<re.length; i++){
                     res+=re[i]+'<br>'
                 }
-                $('#dl_result').html()=res
+                $('#dl_result').html(res)
                 $('#dl_result').show()
             });
     });
